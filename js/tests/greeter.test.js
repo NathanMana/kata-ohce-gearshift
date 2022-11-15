9fa57c3536601c8de0dcd9e857a40319e7d00fb7
unknown <nathan.manaranche@gmail.com>
2022-11-15 09:06:12 +0100

    Test 'should never return undefined' ok v1

--- js/tests/greeter.test.js
+++ js/tests/greeter.test.js
@@ -21,6 +21,16 @@ test('should say "good night" at midnight', (t) => {
 test('should never return undefined', (t) => {
   // TODO : for each hour from 0 to 23, check that Greeter.greet()
   // never returns undefined
-  t.fail('TODO')
+
+  let isReturnUndefined = false;
+
+  for (let i = 0; i < 24; i++) {
+    const greeter = new Greeter(new SystemClock(i));
+    if (!greeter.greet())
+      isReturnUndefined = true;
+  }
+
+  t.notOk(isReturnUndefined);
+
   t.end()
 })
